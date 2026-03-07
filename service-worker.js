db41993d014a9b971cf135f4b20824e00a369347
repalleramr <repalleramr@gmail.com>
2026-03-07 09:31:49 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const CACHE='baccarat-final-v1';const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>self.clients.claim());
